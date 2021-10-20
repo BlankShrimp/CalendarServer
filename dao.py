33199079f6dba6cea4917b557f7a994c090b55eb
BlankShrimp <vanishing.19@gmail.com>
2021-10-20 17:24:38 +0800

    add hash index support

--- dao.py
+++ dao.py
@@ -6,18 +6,19 @@ def init_db_if_not_exists():
     cursor = conn.cursor()
     cursor.execute('''create table if not exists 
         events(timestamp integer primary key, 
-        activite_time integer, 
+        start_time integer, 
         expire_time integer,
-        content text)''')
+        content text, 
+        hashvalue text)''')
     conn.commit()
     conn.close()
 
 
-def insert_entry(timestamp, activision, expiry, content):
+def insert_entry(timestamp, activision, expiry, content, hashvalue):
     conn = sqlite3.connect('calender.db')
     cursor = conn.cursor()
-    params = (timestamp, activision, expiry, content)
-    cursor.execute("insert into events (timestamp, activite_time, expire_time, content) values (?, ?, ?, ?)", params)
+    params = (timestamp, activision, expiry, content, hashvalue)
+    cursor.execute("insert into events (timestamp, start_time, expire_time, content, hashvalue) values (?, ?, ?, ?, ?)", params)
     conn.commit()
     conn.close()
     return
@@ -26,12 +27,12 @@ def insert_entry(timestamp, activision, expiry, content):
 def fetch_all_valid_entries():
     conn = sqlite3.connect('calender.db')
     cursor = conn.cursor()
-    cursor.execute("select * from events where expire_time > %s order by activite_time"%(int(time.time())))
+    cursor.execute("select * from events where expire_time > %s order by start_time"%(int(time.time())))
     data = []
     latest = 0
     for row in cursor:
         latest =  row[0] if row[0] > latest else latest
-        data.append({'activite_time': row[1], 'expire_time': row[2], 'content': row[3]})
+        data.append({'start_time': row[1], 'expire_time': row[2], 'content': row[3], 'hashvalue': row[4]})
     conn.close()
     return latest, data
 
@@ -39,11 +40,31 @@ def fetch_all_valid_entries():
 def sync_new_entries(timestamp):
     conn = sqlite3.connect('calender.db')
     cursor = conn.cursor()
-    cursor.execute("select * from events where timestamp > %s and expire_time > %s order by activite_time"%(timestamp, int(time.time())))
+    cursor.execute("select * from events where timestamp > %s and expire_time > %s order by start_time"%(timestamp, int(time.time())))
     data = []
     latest = 0
     for row in cursor:
         latest =  row[0] if row[0] > latest else latest
-        data.append({'activite_time': row[1], 'expire_time': row[2], 'content': row[3]})
+        data.append({'start_time': row[1], 'expire_time': row[2], 'content': row[3], 'hashvalue': row[4]})
     conn.close()
     return latest, data
+
+
+def fetch_all_valid_hashvalues():
+    conn = sqlite3.connect('calender.db')
+    cursor = conn.cursor()
+    cursor.execute("select hashvalue from events where expire_time > %s order by start_time"%(int(time.time())))
+    data = []
+    for row in cursor:
+        data.append(row[0])
+    conn.close()
+    return data
+
+
+def eliminate_by_hashvalue(hashvalue): 
+    conn = sqlite3.connect('calender.db')
+    cursor = conn.cursor()
+    cursor.execute("update events set expire_time = %s where hashvalue = '%s'"%(int(time.time()), hashvalue))
+    conn.commit()
+    result = True if conn.total_changes != 0 else False
+    return result
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+from hashlib import sha256
 import dao
 import json, time
 
@@ -11,7 +12,7 @@ def begin():
 
 
 @app.route('/events', methods=['GET'])
-def get_events():
+def get_events(): # timestamp(optional)
     args = request.args
     if 'timestamp' in args:
         latest, data = dao.sync_new_entries(args['timestamp'])
@@ -22,13 +23,30 @@ def get_events():
 
 
 @app.route('/add', methods=['POST'])
-def post_event():
+def post_event(): # start_time, expiry, content
     args = request.args
-    if 'activision' not in args or 'expiry' not in args or 'content' not in args:
+    if 'start_time' not in args or 'expiry' not in args or 'content' not in args:
         return Response(status=400)
     else:
-        dao.insert_entry(int(time.time()), args['activision'], args['expiry'], args['content'])
+        hashvalue = sha256((args['start_time'] + args['expiry'] + args['content']).encode('utf-8')).hexdigest()
+        dao.insert_entry(int(time.time()), args['start_time'], args['expiry'], args['content'], hashvalue)
         return Response(status=201)
+
+
+@app.route('/eliminate', methods=['POST'])
+def eliminate_by_hashvalue(): #hashvalue
+    args = request.args
+    if 'hashvalue' not in args:
+        return Response(status=400)
+    else:
+        target_hash = args['hashvalue']
+        list_of_hashvalues = dao.fetch_all_valid_hashvalues()
+        for candidate in list_of_hashvalues:
+            if target_hash == candidate[:len(target_hash)]: 
+                dao.eliminate_by_hashvalue(candidate)
+                return Response(status=200)
+        return Response(status=404)
+
 
 
 if __name__ == "__main__": 
